d2dc41298bb663ce75a5a06554470d82795cee64
WilliamCg97 <conta10@outlook.com>
2024-04-01 13:39:30 -0300

    :D

---
+++ Atividade Aula 4/Atividade5_aula4.py
@@ -0,0 +1,25 @@
+## Desenvolva um programa que leia o comprimento de três retas
+## e diga ao usuário se elas podem ou não formar um triângulo.
+## Condições Necessárias:
+
+# a + b > c
+
+# a + c > b
+
+# b + c > a
+
+
+reta1 = int(input("Digite um numero "))
+reta2 = int(input("Digite um numero "))
+reta3 = int(input("Digite um numero "))
+
+ab = reta1 + reta2
+ac = reta1 + reta3
+bc =  reta2 + reta3
+
+if (ab>reta3) and (ac>reta2) and (bc>reta1) : 
+    print( "é um triangulo ")
+else:
+    print( "nâo é um triangulo ")
+    
+    

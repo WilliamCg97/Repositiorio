1bfa22775aaf7d2ea020821e9726a56837300ec6
WilliamCg97 <conta10@outlook.com>
2024-04-03 17:01:01 -0300

    Update Atividade6_aula6.py

---
+++ Atividade aula 6/Atividade6_aula6.py
@@ -0,0 +1,19 @@
+## DESAFIO 06
+
+## Desenvolva um programa que leia o primeiro termo e a razão
+## de uma PA. No final, mostre os 10 primeiros termos dessa
+## progressão.
+
+## progreçao aritimetica //  
+
+
+
+razao = 3 
+
+termo = 1 
+
+n1 = int(input ("Digite o valor da razão ") )
+n2 = int(input ("Digite o termo "))
+
+##for loop in range (1,10,1) :
+

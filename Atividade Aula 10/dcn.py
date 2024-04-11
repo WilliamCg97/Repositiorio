d755ed67f8c50c3a61b97adf3fb8e803b4cfcb10
WilliamCg97 <conta10@outlook.com>
2024-04-11 16:54:37 -0300

    :D

--- Atividade Aula 10/dcn.py
+++ Atividade Aula 10/dcn.py
@@ -1,9 +1,24 @@
-estado = {}
+#CRIANDO UM DICIONÁRIO DO ZERO usando lista
+estado ={}
 brasil = list()
 
-for c in range (0,2 ):
-    estado['uf'] = str(input("Unidade federativa"))
-    estado ['sigla']= str(input("Sigla do estado"))
+
+for c in range(0,2):
+    estado['uf']= str(input('Unidade federativa: '))
+    estado['sigla']= str(input('Sigla do Estado: '))
     brasil.append(estado.copy())
-    
-print(estado)
+
+
+print(estado)  
+
+
+print(brasil)
+
+
+print('******************')
+
+
+for e in brasil: # percorrer cada dicionario
+    for v in e.values(): # exibir cada valor
+        print(v, end=' ')
+    print(' ')

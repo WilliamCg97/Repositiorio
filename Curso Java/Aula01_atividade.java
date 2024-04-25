531c0f2a737b220ee2d5eb2b1b454e8481d0b9e1
WilliamCg97 <conta10@outlook.com>
2024-04-25 16:37:47 -0300

    :D

--- Curso Java/Aula01_atividade.java
+++ Curso Java/Aula01_atividade.java
@@ -27,3 +27,61 @@
 // 	}
 
 // }
+
+// ex 2: crie outro projeto java utilizando eclipse , que apresente as seguintes operações e seus 
+// resultados: 
+
+// soma  85 + 96
+// subtração  100 – 50 
+// divisão  40/ 8
+// multiplicação  7 * 
+
+
+// package ex2;
+
+// public class ex2 {
+
+// 	public static void main(String[] args) {
+// 		// TODO Auto-generated method stub
+
+// 		int n1 = 85 ;
+// 		int n2 = 96 ;
+		
+// 		int resultado = n1 + n2 ;
+		
+// 		System.out.println("resultado da soma entre " +n1 + " e " + n2 + " : " +resultado);
+// 		int n3 = 100 ;
+// 		int n4 = 50 ;
+		
+// 		int resultado1 = n3 - n4 ;
+		
+// 		System.out.println("resultado da subtração entre " +n3 + " e " + n4 + " : " +resultado1);
+		
+
+// 		int n5 = 40 ;
+// 		int n6 = 8 ;
+		
+// 		int resultado2 = n5 / n6 ;
+		
+// 		System.out.println("resultado da divisão entre " +n5 + " e " + n6 + " : " +resultado2);
+		
+
+// 		int n7 = 7 ;
+// 		int n8 = 6 ;
+		
+// 		int resultado3 = n7 * n8 ;
+		
+// 		System.out.println("resultado da multiplicação entre " +n7 + " e " + n8 + " : " +resultado3);
+		
+		
+		
+		
+// 		// resolução 2 
+		
+// 		int soma= 85 + 96 ;		int subtracao = 100 -50 ; 		int divisao= 40 / 8 ; 		int mult = 7 * 6 ; 
+		
+		
+// 		System.out.println("O resultado da soma de 85 + 96 é " +soma);
+// 		System.out.println("O resultado da subtração de 100 - 50 = " +subtracao);
+// 		System.out.println("O resultado da divisao de 40 por 8 =  " +divisao);
+// 		System.out.println("O resultado da multiplicação  7 * 6  " +mult);

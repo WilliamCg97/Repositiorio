e40769206cca0d4aa184a8c77ce3dd1b9bdd2e2d
WilliamCg97 <conta10@outlook.com>
2024-04-25 14:58:55 -0300

    :D

--- Curso Java/Aula01_atividade.java
+++ Curso Java/Aula01_atividade.java
@@ -1,29 +1,29 @@
-public class principal {
+// public class principal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
-	//	Nome do desenvolvedor // 
-	//	Nome do curso (Programação em oracle Java // ) 
-	//	Carga horaria total do curso / 
-	//	Quantidade de dias de aula // 
-	//	Horário de inicio e termino da aula // 
-	//	Data de previsão de termino // 
+// 	public static void main(String[] args) {
+// 		// TODO Auto-generated method stub
 
 		
-	System.out.println("Nome do programador : William Costa Gomes ");
-	System.out.println("Nome do curso Programação em oracle Java ");
-	System.out.println("Carga horaria total do curso : 80 Horas ");
-	System.out.println("Quantidade de dias de aula : 20 aulas ");
-	System.out.println("Horário de inicio e termino da aula : 13:00 horas");
-	System.out.println("Horario de termino da aula : 17:00 horas ");
-	System.out.println("Data de previsão de termino : 07 / 06 ");
+	        //	Nome do desenvolvedor // 
+	        //	Nome do curso (Programação em oracle Java // ) 
+	        //	Carga horaria total do curso / 
+	        //	Quantidade de dias de aula // 
+	        //	Horário de inicio e termino da aula // 
+        	//	Data de previsão de termino // 
+
+		
+// 	System.out.println("Nome do programador : William Costa Gomes ");
+// 	System.out.println("Nome do curso Programação em oracle Java ");
+// 	System.out.println("Carga horaria total do curso : 80 Horas ");
+// 	System.out.println("Quantidade de dias de aula : 20 aulas ");
+// 	System.out.println("Horário de inicio e termino da aula : 13:00 horas");
+// 	System.out.println("Horario de termino da aula : 17:00 horas ");
+// 	System.out.println("Data de previsão de termino : 07 / 06 ");
 		
 	
 		
 		
 	
-	}
+// 	}
 
-}
+// }
